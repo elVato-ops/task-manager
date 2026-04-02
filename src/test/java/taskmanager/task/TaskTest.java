5d8954e4136369c54aca9d4cf70f91f0708d4dd8
Kamil Sempioło <ksempiolo@gmail.com>
2026-04-02 13:37:48 +0200

    added task constructor test

--- src/test/java/taskmanager/task/TaskTest.java
+++ src/test/java/taskmanager/task/TaskTest.java
@@ -1,5 +1,38 @@
 package taskmanager.task;
 
+import org.junit.jupiter.api.Test;
+import taskmanager.exception.ResourceType;
+import taskmanager.exception.ValidationException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static taskmanager.TestConstants.*;
+
 public class TaskTest
 {
+    @Test
+    public void constructorTest()
+    {
+        ValidationException e = assertThrows(ValidationException.class, () -> new Task("", TASK_STATUS, project(), user()));
+        assertEquals(ResourceType.TASK, e.getResource());
+        assertEquals("Task name must not be empty", e.getMessage());
+
+        e = assertThrows(ValidationException.class, () -> new Task("", TASK_STATUS, project(), user()));
+        assertEquals(ResourceType.TASK, e.getResource());
+        assertEquals("Task name must not be empty", e.getMessage());
+
+        e = assertThrows(ValidationException.class, () -> new Task("Name", null, project(), user()));
+        assertEquals(ResourceType.TASK, e.getResource());
+        assertEquals("Task status must not be null", e.getMessage());
+
+        e = assertThrows(ValidationException.class, () -> new Task("Name", TASK_STATUS, null, user()));
+        assertEquals(ResourceType.TASK, e.getResource());
+        assertEquals("Task project must not be null", e.getMessage());
+
+        Task task = new Task(TASK_NAME, TASK_STATUS, project(), user());
+        assertEquals(TASK_NAME, task.getName());
+        assertEquals(project().getName(), task.getProject().getName());
+        assertEquals(user().getName(), task.getAssignee().getName());
+        assertEquals(TASK_STATUS, task.getStatus());
+    }
 }
